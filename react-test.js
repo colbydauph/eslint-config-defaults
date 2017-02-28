783b6a810ebd4d4c0dc2c7b2611fed5ea08ca8bf
Colby Dauphinais <colby@reserve.com>
2017-02-28 12:41:45 -0800

    added rules, bumped dep versions, added lint script, added babel parser

--- react-test.js
+++ react-test.js
@@ -14,6 +14,7 @@ module.exports = {
 
   rules: {
     'no-console': [0],
+    'max-statements': [1, 50],
   },
 
 };
--- rules/no-js-error.js
+++ rules/no-js-error.js
@@ -1,22 +1,18 @@
 'use strict';
 
-module.exports = (context) => {
-
-  return {
-    Identifier(node) {
-      if (node.name === 'Error') {
-        context.report({
-          node,
-          message: 'Class: "Error" not allowed.',
-          fix(fixer) {
-            return fixer.replaceText(node, 'ApiError');
-          },
-        });
-      }
-    },
-  };
-
-};
+module.exports = (context) => ({
+  Identifier(node) {
+    if (node.name === 'Error') {
+      context.report({
+        node,
+        message: 'Class: "Error" not allowed.',
+        fix(fixer) {
+          return fixer.replaceText(node, 'ApiError');
+        },
+      });
+    }
+  },
+});
 
 module.exports.schema = [
   // JSON Schema for rule options goes here
